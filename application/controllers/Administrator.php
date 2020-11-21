21f5a760c27f1049e13890991f65ae3a846674cf
lattachai25 <lattachai25@gmail.com>
2020-11-21 20:34:27 +0700

    1

---
+++ application/controllers/Administrator.php
@@ -0,0 +1,99 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class administrator extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->load->database();
+		$this->load->helper('url');
+		$this->load->library('grocery_CRUD');
+	}
+
+	public function index()
+	{
+	echo	$this->session->userdata('username');
+		$data = array( 'title' => 'Administrator ::   ',
+								   'description' => "",
+								   'keyword' => '',
+								   'head' =>   '',
+								   'content' => 'home_admin_view',
+								  // 'queryPublic' => $this->pub->get_about_add()
+
+								);
+
+		$this->load->view('Template_admin',$data);
+		}
+
+function statusDis($value)
+{
+	if($value === 'เปิด' || $value === ''){
+
+
+		  return "<center><img src='".base_url()."assets/uploads/icon_status_green.png'></center>";
+	}else{
+
+		return "<center><img src='".base_url()."assets/uploads/icon_status_red.png'></center>";
+
+	}
+}
+
+
+
+function items()
+	{
+			$crud = new grocery_CRUD();
+			$crud->set_theme("bootstrap");
+			$crud->set_table('items')
+
+			->order_by('id','title','description')
+			->set_subject('items')
+			->display_as('title','Title')
+			->display_as('description','Description');
+
+				// $crud->field_type('status','dropdown',array('1' => 'Active', '0' => 'Inactive'));
+				// $crud->set_field_upload('name_img','assets/uploads/intro');
+				$output = $crud->render();
+				$this->_example_output($output);
+		}
+
+
+		function about()
+		{
+				$crud = new grocery_CRUD();
+				$crud->set_theme("bootstrap");
+				$crud->set_table('about')
+	
+				->order_by('id','text','img_name')
+				->set_subject('About Content')
+				->display_as('text','Content')
+				->display_as('img_name','Images ')
+				->display_as('date','Day ');
+
+	
+				    $crud->field_type('status','dropdown',array('1' => 'Active', '0' => 'Inactive'));
+					$crud->set_field_upload('img_name','assets/uploads/About');
+					$output = $crud->render();
+					$this->_example_output($output);
+			}
+
+
+
+
+
+
+
+		public function _example_output($output = null)
+		{
+			$this->load->view('example.php',$output);
+		}
+
+
+
+
+}
+
+
+
+
---
+++ application/views/Menu_admin_view.php
@@ -0,0 +1,107 @@
+<style>
+.circle {
+  border-radius: 0%;
+  width: 100%;
+  height: auto;
+
+  margin-left: 0px;
+  /* width and height can be anything, as long as they're equal */
+  margin-top: 25px;
+  margin-bottom: 30px;
+  position: relative;
+}
+
+.navbar-default {
+    background-color: #d2d2d2;
+    border-color: #100f0f;
+}
+
+#page-wrapper {
+    padding: 0 15px;
+    min-height: 568px;
+    background-color: #d2d2d2;
+}
+a {
+    color: #090909  !important;
+    text-decoration: none;
+    font-size: 14px;
+}
+
+a:hover {
+    color: #090909 !important;
+    text-decoration: underline;
+}
+
+</style>
+<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="<?php echo base_url($this->uri->segment(1).'/administrator');?>"  style="color:#ff7500 !important;">Administrator | </a>
+    </div>
+    <!-- /.navbar-header -->
+
+    <ul class="nav navbar-top-links navbar-right">
+
+        <!-- /.dropdown -->
+        <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
+            </a>
+            <ul class="dropdown-menu dropdown-user">
+
+                <li><a href="<?php echo base_url('/admin/logout');?>"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                </li>
+            </ul>
+            <!-- /.dropdown-user -->
+        </li>
+        <!-- /.dropdown -->
+    </ul>
+    <!-- /.navbar-top-links -->
+
+    <div class="navbar-default sidebar" role="navigation">
+        <div class="sidebar-nav navbar-collapse">
+            <ul class="nav" id="side-menu">
+              <li class="sidebar-search" style='text-align:center;'>
+
+                <img class='circle' src='<?php echo base_url();?>/assets/images/logo.png'>
+                    <div class="" >
+                     USERNAME :  <font color='#ff7500'> <?php echo $this->session->userdata['logged_in']['username'];?> </font>
+                    </div>
+
+                </li>
+                <li>
+                    <a href="<?php echo base_url('administrator');?>"><i class="fa fa-home fa-fw"></i> HOME</a>
+                </li>
+
+                <li>
+                    <a href="<?php echo base_url('administrator/about');?>"><i class="fa fa-fire fa-fw"></i> About Us</a>
+                </li>
+                <li>
+                    <a href="<?php echo base_url('administrator/product');?>"><i class=" glyphicon glyphicon-transfer "></i> Product</a>
+                </li>    
+                <li>
+                    <a href="#"><i class="glyphicon glyphicon-folder-open"></i> Product<span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level">
+                        <li>
+                            <a href="<?php echo base_url('administrator/product1');?>"> <i class="fa fa-circle-o fa-fw"></i>Product 1</a>
+                        </li>
+                        <li>
+                            <a href="<?php echo base_url('administrator/product2');?>">  <i class="fa fa-circle-o fa-fw"></i>Product 2</a>
+                        </li>
+                    </ul>    
+                </li>
+                <li>
+                    <a href="<?php echo base_url('administrator/news');?>"><i class="fa fa-fire fa-fw"></i>News</a>
+                </li>
+                <li>
+                    <a href="<?php echo base_url('administrator/items');?>"><i class="glyphicon glyphicon-map-marker"></i> Item</a>
+                </li>
+                <li>
+                    <a href="<?php echo base_url('administrator/test');?>"><i class="fa fa-fire fa-fw"></i>test</a>
+                </li> 
+</nav>
