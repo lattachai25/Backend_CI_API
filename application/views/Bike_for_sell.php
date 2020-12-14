e2c249c4876885b3f29d6c97012e1d4740e1e0e5
lattachai25 <lattachai25@gmail.com>
2020-12-14 12:20:16 +0700

    update Bike-you-sell 2

--- application/views/Bike_for_sell.php
+++ application/views/Bike_for_sell.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.0/assets/owl.carousel.css">
 
 <style>
 
@@ -77,6 +78,8 @@
 .boximage{
   width:100%;
   height:250px;
+  margin-left: 0px;
+  padding: 1px;
   display: inline-block;
 }
 
@@ -91,23 +94,88 @@
   width: 287px;
   height: auto;
 }
+.image2 {
+  width: 204px;
+  height:140px;
+  object-fit:cover;
+}
 
-.overlay {
+.overlay1 {
   position: absolute;
   top: 0;
   bottom: 0;
-  left: 13px;
+  left: 0px;
   right: 0;
-  height: %;
-  width: 287px;
+  height: 56%;
+  width: 100%;
   opacity: 0;
   transition: .5s ease;
   background-color: #f35a24;
 }
 
-.container:hover .overlay {
+.container1:hover .overlay1 {
   opacity: 0.8;
 }
+
+
+
+.overlay2 {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0px;
+  right: 0;
+  height: 56%;
+  width: 100%;
+  opacity: 0;
+  transition: .5s ease;
+  background-color: #f35a24;
+}
+
+.container2:hover .overlay2 {
+  opacity: 0.8;
+}
+
+.overlay3 {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0px;
+  right: 0;
+  height: 56%;
+  width: 100%;
+  opacity: 0;
+  transition: .5s ease;
+  background-color: #f35a24;
+}
+
+.container3:hover .overlay3 {
+  opacity: 0.8;
+}
+
+
+.overlay4 {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0px;
+  right: 0;
+  height: 56%;
+  width: 100%;
+  opacity: 0;
+  transition: .5s ease;
+  background-color: #f35a24;
+}
+
+.container4:hover .overlay4 {
+  opacity: 0.8;
+}
+
+
+
+
+
+
 
 .text {
   width:200px;
@@ -139,12 +207,9 @@ $query = $this->db->get();
 $bike = $query->result();
 ?>
 
-<?php
-$this->db->select("*");
-$this->db->from("brand");
-$query = $this->db->get();
-$brand = $query->result();
-?>
+
+
+
 
 
 
@@ -164,19 +229,24 @@ $brand = $query->result();
 <div class="row"> <!--  ROW --->
   <div class="col-1"></div>
   <div class="col-11">
-      <div id="owl-demo" class="owl-carousel owl-theme"> 
-      <ul class="nav nav-tabs">
+  <section class="my-slide">
+      <ul class="row owl-carousel owl-theme nav nav-tabs">
         <?php foreach($bike as $bikes): ?>
 
-          <li><a data-toggle="tab" href="#home<?php echo $bikes->id; ?>">
-                  <img src="<?php echo base_url();?>assets/uploads/img_promotion_product/<?php echo $bikes->img1;?>" width="10%" alt="" />
+          <li class="owl-item">
+            <div class="item-cat">
+              <a data-toggle="tab" href="#home<?php echo $bikes->id; ?>">
+                  <img src="<?php echo base_url();?>assets/uploads/img_promotion_product/<?php echo $bikes->img1;?>" width="100%" alt="" />
               </a>
+            </div>  
           </li>    
 
         <?php endforeach; ?>
+
+
       </ul>
-      
-      </div>
+  </section> 
+ 
   </div>
   <div class="col-2"></div>
   <div class="col-6" style="font-size:20px; font-weight:bold; color:#fff;">
@@ -194,20 +264,41 @@ $brand = $query->result();
 <div class="tab-content">
   <?php foreach($bike as $bikes): ?>
 
-
-
-
     <div id="home<?php echo $bikes->id;?>" class="tab-pane fade">
     <div class="row">
       <div class="boxcontent2" style="position:relative;">
       <img src="<?php echo base_url();?>img/product/bike/bg_boke.png" width="100%" alt=""/ >
       </div>
  <!-- boxcontent -->
+
+
  <div class="boxcontent" style="min-height:800px; position:absolute;">
  <div class="row" style="margin-top:115px;">
       <div class="col-3">
-        <div class="text_right"><i>PANIGALE V4S 2018</i></div>
-        <div class="text_left"><i>DUCATI</i></div>
+
+
+      <?php $id_model = $bikes->model; ?>
+      <?php
+        $this->db->select("*");
+        $this->db->from("model");
+        $this->db->where("model.id",$id_model);
+        $query = $this->db->get();
+        $model= $query->row();
+        ?>
+
+      <?php $id_brand = $bikes->brand; ?>
+      <?php
+      $this->db->select("*");
+      $this->db->from("brand");
+      $this->db->where("brand.id",$id_brand);
+      $query = $this->db->get();
+      $brand = $query->row();
+      ?>
+
+
+
+       <div class="text_right"><i><?php echo $model->name;?></i></div> 
+       <div class="text_left"><i><?php echo $brand->name;?></i></div>
       </div>
         <!-- box scall-->
         <div class="col-8"> 
@@ -220,7 +311,7 @@ $brand = $query->result();
             <div class="col-5">
             <div class="row"><!---- row  ----->
               <div class="col-12">
-                  <div class="text_title_product"> <?php echo $bikes->brand;?> <br> <?php echo $bikes->model;?> </div>
+                  <div class="text_title_product"> <?php echo $brand->name;?> <br> <?php echo $model->name;?> </div>
                   <div class="text_price_cut"> <s><?php echo $bikes->discount;?> BAHT</s> </div>
                   <div class="text_price"> <?php echo $bikes->prict;?> BAHT </div>
                   <br><br>
@@ -241,6 +332,7 @@ $brand = $query->result();
                       <br><br>
                   </div> 
               </div>
+              
             </div><!---- End col-12  ----->
 
 
@@ -250,18 +342,17 @@ $brand = $query->result();
           <!--End row scall -->
         </div>
         <!-- End box scall-->
-        <div class="row"  style="margin-top:-140px;">
+        <div class="row"  style="width:100%; margin-top:-140px;">
           <div class="col-3"></div>
-          <div class="col-8"><!--col-8-->
-          <div class="col-12"> <!--col-12-->
+          <div class="col-9"><!--col-8-->
           <div class="row"> <!--row-->
 
           <!-- box gallery 1-->
           <div class="boximage col-3">
-            <div class="container">
+            <div class="container1">
               <a data-fancybox="gallery" href ="<?php echo base_url();?>assets/uploads/img_promotion_product/<?php echo $bikes->img2;?>"> 
-              <img src="<?php echo base_url();?>assets/uploads/img_promotion_product/<?php echo $bikes->img2;?>" width="100%" class="image" alt="" />
-                <div class="overlay">
+              <img src="<?php echo base_url();?>assets/uploads/img_promotion_product/<?php echo $bikes->img2;?>" width="100%"  class="image2"alt="" />
+                <div class="overlay1">
                   <div class="text">FULL IMAGE</div>
                 </div>
                 </a>  
@@ -271,10 +362,10 @@ $brand = $query->result();
 
           <!-- box gallery 2-->
           <div class="boximage col-3">
-            <div class="container">
+            <div class="container2">
               <a data-fancybox="gallery" href ="<?php echo base_url();?>assets/uploads/img_promotion_product/<?php echo $bikes->img3;?>"> 
-              <img src="<?php echo base_url();?>assets/uploads/img_promotion_product/<?php echo $bikes->img3;?>" width="100%" class="image" alt="" />
-                <div class="overlay">
+              <img src="<?php echo base_url();?>assets/uploads/img_promotion_product/<?php echo $bikes->img3;?>" width="100%"  class="image2" alt="" />
+                <div class="overlay2">
                   <div class="text">FULL IMAGE</div>
                 </div>
                 </a>  
@@ -282,32 +373,32 @@ $brand = $query->result();
           </div>
           <!-- box gallery 2-->
           <!-- box gallery 3-->
-          <!-- <div class="boximage col-3">
-            <div class="container">
+          <div class="boximage col-3">
+            <div class="container3">
               <a data-fancybox="gallery" href ="<?php echo base_url();?>assets/uploads/img_promotion_product/<?php echo $bikes->img4;?>"> 
-              <img src="<?php echo base_url();?>assets/uploads/img_promotion_product/<?php echo $bikes->img4;?>" width="100%" class="image" alt="" />
-                <div class="overlay">
+              <img src="<?php echo base_url();?>assets/uploads/img_promotion_product/<?php echo $bikes->img4;?>" width="100%"  class="image2"alt="" />
+                <div class="overlay3">
                   <div class="text">FULL IMAGE</div>
                 </div>
                 </a>  
             </div>
-          </div> -->
+          </div>
           <!-- box gallery 3-->
           <!-- box gallery 3-->
-          <!-- <div class="boximage col-3">
-            <div class="container">
+          <div class="boximage col-3">
+            <div class="container4">
               <a data-fancybox="gallery" href ="<?php echo base_url();?>assets/uploads/img_promotion_product/<?php echo $bikes->img5;?>"> 
-              <img src="<?php echo base_url();?>assets/uploads/img_promotion_product/<?php echo $bikes->img5;?>" width="100%" class="image" alt="" />
-                <div class="overlay">
+              <img src="<?php echo base_url();?>assets/uploads/img_promotion_product/<?php echo $bikes->img5;?>" width="100%"  class="image2" alt="" />
+                <div class="overlay4">
                   <div class="text">FULL IMAGE</div>
                 </div>
                 </a>  
             </div>
-          </div> -->
+          </div>
           <!-- box gallery 3-->
 
           </div><!-- end row -->
-          </div><!-- end  col-12-->
+
         </div><!-- end col-8 -->
 
         </div>
@@ -326,17 +417,25 @@ $brand = $query->result();
     <?php endforeach; ?>    
 </div>
 </div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.0/owl.carousel.min.js"></script>
 <script>
-$(document).ready(function() {
- var owl = $("#owl-demo");
- owl.owlCarousel({
-     items : 4,
-     itemsDesktop : [1000,4],
-     itemsDesktopSmall : [900,4],
-     itemsTablet: [600,4],
-     itemsMobile : false,
- });
+$('.owl-carousel').owlCarousel({
 
+    margin:5,
+    nav:true,
 
-});
+    responsive:{
+        0:{
+            items:4
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:4
+        }
+    }
+   });
 </script>
